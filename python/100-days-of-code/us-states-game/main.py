cbb033fc450d18716e61f0d81962405a77fde4e3
Karl Tarbet <karl.l.tarbet@usace.army.mil>
2024-01-23 19:04:48 -0800

    using list comprehension in states game

--- python/100-days-of-code/us-states-game/main.py
+++ python/100-days-of-code/us-states-game/main.py
@@ -9,17 +9,20 @@ screen.addshape(image)
 turtle.shape(image)
 
 df = pandas.read_csv("50_states.csv")
-states = df.state
+states = df.state.to_list()
 
 answers = []
 
-def write_state_name(x,y,name):
+
+def write_state_name(x, y, name):
     txt = turtle.Turtle()
     txt.hideturtle()
     txt.penup()
-    txt.goto(x,y)
+    txt.goto(x, y)
     txt.pendown()
-    txt.write(f"{name}", align='center',font=("Courier", 12, "normal"))
+    txt.write(f"{name}", align='center', font=("Courier", 12, "normal"))
+
+
 def check_state_name(state_name: str):
     rows = df[df.state.str.contains(state_name, case=False)]
     if rows.empty:
@@ -28,25 +31,29 @@ def check_state_name(state_name: str):
         x = float(rows.x)
         y = float(rows.y)
         print(f"x= {x} y = {y}")
-        write_state_name(x,y,state_name)
+        write_state_name(x, y, state_name)
         answers.append(state_name)
+
 
 state_name = ""
 while state_name is not None:
     state_name = screen.textinput(title=f"{len(answers)}/50 correct", prompt="Enter another State Name?")
     if state_name is None:
         break
+    state_name = state_name.title()  # Title Case
     check_state_name(state_name)
 
 # write missing states to a csv file
 
 
-remaining_states = df[~df['state'].str.lower().isin([s.lower() for s in answers])]
+#remaining_states = df[~df['state'].str.lower().isin([s.lower() for s in answers])]
+remaining_states = [ s for s in states if s not in answers]
+remaining_states_df = pandas.DataFrame(remaining_states)
+remaining_states_df.to_csv("remaining_states.csv")
 
-remaining_states.to_csv("remaining_states.csv")
 print(remaining_states)
 
-#turtle.mainloop()
+# turtle.mainloop()
 
 # code to find coordinates based on click.
 # def get_mouse_click_coord(x,y):
