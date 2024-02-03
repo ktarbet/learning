879d2e2652e4a8ee6358e4d8b6a6f0fc8c80eebc
Karl Tarbet <karl.l.tarbet@usace.army.mil>
2024-02-03 08:17:11 -0800

    finish Pomodoro

--- python/100-days-of-code/pomodoro/main.py
+++ python/100-days-of-code/pomodoro/main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import math
+
 # ---------------------------- CONSTANTS ------------------------------- #
 # https://colorhunt.co
 
@@ -12,26 +13,52 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 CHECK_MARK = "✓"
+reps = 1
+timer = ""
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
 
 def reset():
-    pass
+    global reps, timer, timer_text, check, label
+    window.after_cancel(timer)
+    reps = 0
+    canvas.itemconfig(timer_text, text="00:00")
+    check.config(text="")
+    label.config(text="Timer")
 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 def start():
-    count_down(WORK_MIN * 60)
+    global reps, label
+    if reps in [1, 3, 5, 7]:
+        label.config(text="Working", fg=GREEN)
+        count_down(WORK_MIN * 60)
+    elif reps in [8]:
+        label.config(text="Long Break", fg=RED)
+        count_down(LONG_BREAK_MIN * 60)
+    elif reps in [2, 4, 6]:
+        label.config(text="Short Break", fg=PINK)
+        count_down(SHORT_BREAK_MIN * 60)
+
+    reps += 1
+    if reps > 8:
+        reps = 1
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(start_count):
     minutes = math.floor(start_count / 60)
     seconds = start_count % 60
-    canvas.itemconfig(timer_text, text=f"{minutes}:{seconds}")
+    canvas.itemconfig(timer_text, text=f"{minutes}:{seconds:02d}")
     if start_count >= 0:
-        window.after(1000, count_down, start_count - 1)
+        global timer
+        timer = window.after(1000, count_down, start_count - 1)
+    else:
+        start()
+        num_checks = math.floor(reps / 2)
+        print(CHECK_MARK * num_checks)
+        check.config(text=CHECK_MARK * num_checks, fg=GREEN)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -55,7 +82,8 @@ start_button.grid(column=0, row=2)
 reset_button = Button(text="Reset", command=reset, font=(FONT_NAME, 16, "bold"), bg="white")
 reset_button.grid(column=2, row=2)
 
-check = Label(text=CHECK_MARK, fg=GREEN, font=("Times New Roman", 12, "bold"))
+check = Label(text="", fg=GREEN, font=("Times New Roman", 12, "bold"))
+
 check.grid(column=1, row=3)
 
 window.mainloop()
