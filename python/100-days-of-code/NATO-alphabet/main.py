3098e85aedf2fcc19c8e590e69a34b2eed4ccf1d
Karl Tarbet <karl.l.tarbet@usace.army.mil>
2024-02-08 19:16:56 -0800

    practice exception handling in NATO words program

--- python/100-days-of-code/NATO-alphabet/main.py
+++ python/100-days-of-code/NATO-alphabet/main.py
@@ -2,14 +2,15 @@ import pandas
 
 df = pandas.read_csv('nato_phonetic_alphabet.csv')
 
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
-
-# {"A": "Alfa", "B": "Bravo"}
 nato = {row.letter: row.code for (index, row) in df.iterrows()}
-print('startup')
-word = input("enter word:").upper()
-result = [nato[c] for c in word]
-print(result)
-# for letter in list(word):
-#     print(f"{letter} : {nato[letter.upper()]}")
+print(nato)
+
+done = False
+while not done:
+    word = input("enter word:").upper()
+    try:
+        result = [nato[c] for c in word]
+        print(result)
+        done = True
+    except KeyError:
+        print("Oops, only enter letters")
