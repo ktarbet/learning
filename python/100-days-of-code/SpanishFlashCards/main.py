55aaab93a57792c8942ab8f48c7e9f7faaa1bae5
Karl Tarbet <ktarbet@protonmail.com>
2024-02-17 20:17:10 -0800

    implement persistence in Spanish Flash cards

--- python/100-days-of-code/SpanishFlashCards/main.py
+++ python/100-days-of-code/SpanishFlashCards/main.py
@@ -1,16 +1,47 @@
+import os.path
 from tkinter import PhotoImage, Tk, Canvas, Button
+import pandas
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
 FONT = ("Ariel", 40, "italic")
 FONT_BIG = ("Ariel", 60, "italic")
+filename = 'data/words_to_learn.csv'
+if not os.path.exists(filename):
+    filename = 'data/spanish-words.csv'
+data = pandas.read_csv(filename)
+language_list = data.to_dict(orient='records')
+global word
+
+
+def new_word():
+    global flip_timer, word
+    window.after_cancel(flip_timer)
+    word = random.choice(language_list)
+    canvas.itemconfig(text_title, text="Spanish", fill='black')
+    canvas.itemconfig(text_word, text=word['Spanish'], fill='black')
+    canvas.itemconfig(img, image=card_front)
+    flip_timer = window.after(3000, flip)
 
 
 def wrong():
-    pass
+    new_word()
 
 
 def ok():
-    pass
+    language_list.remove(word)
+    df = pandas.DataFrame(language_list)
+    df.to_csv('data/words_to_learn.csv', index=False)
+    new_word()
+
+
+def flip():
+    global flip_timer
+    window.after_cancel(flip_timer)
+    canvas.itemconfig(text_title, text='English', fill="white")
+    canvas.itemconfig(text_word, text=word['English'], fill="white")
+    canvas.itemconfig(img, image=card_back)
+    flip_timer = window.after(3000, flip)
 
 
 window = Tk()
@@ -21,10 +52,12 @@ card_front = PhotoImage(file="images/card_front.png")
 card_back = PhotoImage(file="images/card_back.png")
 
 canvas = Canvas(width=800, height=526)
-canvas.create_image(400, 263, image=card_front)
+img = canvas.create_image(400, 263, image=card_front)
+flip_timer = window.after(3000, flip)
+
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
-canvas.create_text(400, 150, text="abc", font=FONT)
-canvas.create_text(400, 263, text="word", font=FONT_BIG)
+text_title = canvas.create_text(400, 150, text="Spanish", font=FONT)
+text_word = canvas.create_text(400, 263, text="word", font=FONT_BIG)
 
 canvas.grid(column=0, row=0, columnspan=2)
 
@@ -36,4 +69,5 @@ right_img = PhotoImage(file="images/right.png")
 ok_button = Button(command=ok, image=right_img, highlightthickness=0)
 ok_button.grid(column=1, row=1)
 
+new_word()
 window.mainloop()
