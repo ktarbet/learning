0900020f3b84bd14d958ebf8bb16c86032f1db02
Karl Tarbet <ktarbet@protonmail.com>
2024-06-04 20:12:30 -0700

    high -low game, super simple flask app

--- python/100-days-of-code/Flask/higher_lower.py
+++ python/100-days-of-code/Flask/higher_lower.py
@@ -1,12 +1,24 @@
+import random
+
 from flask import Flask
 
 app = Flask(__name__)
 
+rand_value = random.randint(0,9)
+
+@app.route("/<int:number>")
+def guess(number):
+    if number < rand_value:
+        return '<h1>too Low!</h1>'
+    if number > rand_value:
+        return '<h1>too High!</h1>'
+    return '<h1>Correct!</h1>'
+
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>" \
-       '<p> more text </p>'
+def root():
+    return '<h1>Guess a number between 0 and 9</h1>' \
+            f'<img src="/static/number.gif" alt="Number Image"> '
 
 
 
